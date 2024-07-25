e6202b148529cd30da9a6ac303397dc081cbf1aa
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-07-25 16:07:45 +0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,202 +1,127 @@
-import { Client, Events, GatewayIntentBits, Partials } from "discord.js";
+import {
+  Client,
+  Events,
+  GatewayIntentBits,
+  Partials,
+  Role,
+  GuildMember,
+  MessageReaction,
+  PartialMessageReaction,
+  User,
+  PartialUser,
+} from "discord.js";
 import { config } from "dotenv";
 
 config();
 
-//https://discord.com/api/oauth2/authorize?client_id=1089840702378229770&permissions=268502016&scope=bot
+const ROLE_MAPPINGS: { [key: string]: string } = {
+  "🏴‍☠️": "933676779296657478",
+  "🇷🇴": "1006897148887048313",
+  "🇨🇳": "1012341901908775003",
+  "🇯🇵": "1012342090493079583",
+  "🇰🇷": "1012342563509903381",
+  "🇷🇺": "1012342777088065628",
+  "🇹🇷": "1012342957925474344",
+  "🇹🇭": "1012343210451931176",
+  "🇪🇸": "1012343524324298834",
+  "🇮🇹": "1012343606327119943",
+  "🇵🇱": "1012344079302013050",
+  "🇩🇪": "1092392476809121832",
+  "🇮🇱": "1129001798045286500",
+  "👁️": "1006251927081853038",
+};
+
+const CHANNEL_MESSAGE_MAPPINGS: { [key: string]: string } = {
+  "935819912520286238": "935821058072805416",
+  "1006896489089486929": "1012360723487281153",
+};
 
 async function main() {
   const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildMessageReactions,
+    ],
     partials: [Partials.Message, Partials.Channel, Partials.Reaction],
   });
 
-  client.once("ready", () => {
-    console.log("Ready!");
-  });
+  client.once(Events.ClientReady, () => console.log("Ready!"));
 
-  client.on(Events.MessageReactionAdd, async (reaction, user) => {
-    if (reaction.partial) {
-      try {
-        await reaction.fetch();
-      } catch (error) {
-        console.error("Something went wrong when fetching the message:", error);
-        return;
-      }
-    }
+  client.on(Events.MessageReactionAdd, (reaction, user) =>
+    handleReaction(reaction, user, true),
+  );
+  client.on(Events.MessageReactionRemove, (reaction, user) =>
+    handleReaction(reaction, user, false),
+  );
 
-    const channel = await reaction.message.channel.fetch();
-
-    if (
-      channel.id == "935819912520286238" &&
-      reaction.message.id == "935821058072805416" &&
-      reaction.emoji.name == "🏴‍☠️"
-    ) {
-      const guildMember = await reaction.message?.guild?.members.fetch(user.id);
-      const role = await reaction.message?.guild?.roles.fetch("933676779296657478");
-      guildMember?.roles.add(role!);
-    }
-
-    if (channel.id == "1006896489089486929" && reaction.message.id == "1012360723487281153") {
-      const guildMember = await reaction.message?.guild?.members.fetch(user.id);
-      let role = await reaction.message?.guild?.roles.fetch("1006251927081853038");
-      switch (reaction.emoji.name) {
-        case "🇷🇴": {
-          role = await reaction.message?.guild?.roles.fetch("1006897148887048313");
-          break;
-        }
-        case "🇨🇳": {
-          role = await reaction.message?.guild?.roles.fetch("1012341901908775003");
-          break;
-        }
-
-        case "🇯🇵": {
-          role = await reaction.message?.guild?.roles.fetch("1012342090493079583");
-          break;
-        }
-
-        case "🇰🇷": {
-          role = await reaction.message?.guild?.roles.fetch("1012342563509903381");
-          break;
-        }
-
-        case "🇷🇺": {
-          role = await reaction.message?.guild?.roles.fetch("1012342777088065628");
-          break;
-        }
-
-        case "🇹🇷": {
-          role = await reaction.message?.guild?.roles.fetch("1012342957925474344");
-          break;
-        }
-
-        case "🇹🇭": {
-          role = await reaction.message?.guild?.roles.fetch("1012343210451931176");
-          break;
-        }
-
-        case "🇪🇸": {
-          role = await reaction.message?.guild?.roles.fetch("1012343524324298834");
-          break;
-        }
-
-        case "🇮🇹": {
-          role = await reaction.message?.guild?.roles.fetch("1012343606327119943");
-          break;
-        }
-
-        case "🇵🇱": {
-          role = await reaction.message?.guild?.roles.fetch("1012344079302013050");
-          break;
-        }
-
-        case "🇩🇪": {
-          role = await reaction.message?.guild?.roles.fetch("1092392476809121832");
-          break;
-        }
-
-        case "🇮🇱": {
-          role = await reaction.message?.guild?.roles.fetch("1129001798045286500");
-          break;
-        }
-
-        case "👁️": {
-          role = await reaction.message?.guild?.roles.fetch("1006251927081853038");
-          break;
-        }
-      }
-
-      guildMember?.roles.add(role!);
-    }
-  });
-
-  client.on(Events.MessageReactionRemove, async (reaction, user) => {
-    if (reaction.partial) {
-      try {
-        await reaction.fetch();
-      } catch (error) {
-        console.error("Something went wrong when fetching the message:", error);
-        return;
-      }
-    }
-
-    const channel = await reaction.message.channel.fetch();
-
-    if (channel.id == "1006896489089486929" && reaction.message.id == "1012360723487281153") {
-      const guildMember = await reaction.message?.guild?.members.fetch(user.id);
-
-      let role = await reaction.message?.guild?.roles.fetch("1006251927081853038");
-      switch (reaction.emoji.name) {
-        case "🇷🇴": {
-          role = await reaction.message?.guild?.roles.fetch("1006897148887048313");
-          break;
-        }
-        case "🇨🇳": {
-          role = await reaction.message?.guild?.roles.fetch("1012341901908775003");
-          break;
-        }
-
-        case "🇯🇵": {
-          role = await reaction.message?.guild?.roles.fetch("1012342090493079583");
-          break;
-        }
-
-        case "🇰🇷": {
-          role = await reaction.message?.guild?.roles.fetch("1012342563509903381");
-          break;
-        }
-
-        case "🇷🇺": {
-          role = await reaction.message?.guild?.roles.fetch("1012342777088065628");
-          break;
-        }
-
-        case "🇹🇷": {
-          role = await reaction.message?.guild?.roles.fetch("1012342957925474344");
-          break;
-        }
-
-        case "🇹🇭": {
-          role = await reaction.message?.guild?.roles.fetch("1012343210451931176");
-          break;
-        }
-
-        case "🇪🇸": {
-          role = await reaction.message?.guild?.roles.fetch("1012343524324298834");
-          break;
-        }
-
-        case "🇮🇹": {
-          role = await reaction.message?.guild?.roles.fetch("1012343606327119943");
-          break;
-        }
-
-        case "🇵🇱": {
-          role = await reaction.message?.guild?.roles.fetch("1012344079302013050");
-          break;
-        }
-
-        case "🇩🇪": {
-          role = await reaction.message?.guild?.roles.fetch("1092392476809121832");
-          break;
-        }
-
-        case "🇮🇱": {
-          role = await reaction.message?.guild?.roles.fetch("1129001798045286500");
-          break;
-        }
-
-        case "👁️": {
-          role = await reaction.message?.guild?.roles.fetch("1006251927081853038");
-          break;
-        }
-      }
-
-      guildMember?.roles.remove(role!);
-    }
-  });
-
-  client.login(process.env.TOKEN!);
+  await client.login(process.env.TOKEN);
 }
 
-main();
+async function handleReaction(
+  reaction: MessageReaction | PartialMessageReaction,
+  user: User | PartialUser,
+  isAdding: boolean,
+) {
+  // If the reaction is partial, try to fetch it
+  if (reaction.partial) {
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      console.error("Something went wrong when fetching the reaction:", error);
+      return;
+    }
+  }
+
+  // If the user is partial, try to fetch it
+  if (user.partial) {
+    try {
+      await user.fetch();
+    } catch (error) {
+      console.error("Something went wrong when fetching the user:", error);
+      return;
+    }
+  }
+
+  const channel = reaction.message.channel;
+  const expectedMessageId = CHANNEL_MESSAGE_MAPPINGS[channel.id];
+
+  if (expectedMessageId && reaction.message.id === expectedMessageId) {
+    const roleId = ROLE_MAPPINGS[reaction.emoji.name ?? ""];
+    if (roleId) {
+      const guild = reaction.message.guild;
+      if (guild) {
+        try {
+          const guildMember = await guild.members.fetch(user.id);
+          const role = await guild.roles.fetch(roleId);
+          if (role) {
+            await updateMemberRole(guildMember, role, isAdding);
+          }
+        } catch (error) {
+          console.error("Error fetching guild member or role:", error);
+        }
+      }
+    }
+  }
+}
+
+async function updateMemberRole(
+  guildMember: GuildMember,
+  role: Role,
+  isAdding: boolean,
+) {
+  try {
+    if (isAdding) {
+      await guildMember.roles.add(role);
+      console.error(`Added role ${role} to ${guildMember.nickname}`);
+    } else {
+      console.error(`Removed role ${role} from ${guildMember.nickname}`);
+    }
+  } catch (error) {
+    console.error(`Error ${isAdding ? "adding" : "removing"} role:`, error);
+  }
+}
+
+main().catch((error) =>
+  console.error("Unhandled error in main function:", error),
+);
